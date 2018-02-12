7846ed6185c7f1b73427f84b8d571839e37b34f9
Leszek Pietrzak <smutnyleszek@gmail.com>
2018-02-12 13:44:16 +0100

    some test data for karma

---
+++ test/testData.js
@@ -0,0 +1,38 @@
+angular.module("testAppModule").constant("testData", {
+  list: [
+    {
+      artist: "Kendrick Lamar",
+      title: "To Pimp a Butterfly",
+      decade: "2010s",
+      year: 2015,
+      stars: 4,
+      link:
+        "https://rateyourmusic.com/release/album/kendrick-lamar/to-pimp-a-butterfly-1/"
+    },
+    {
+      artist: "Kendrick Lamar",
+      title: "good kid, m.A.A.d city",
+      decade: "2010s",
+      year: 2012,
+      stars: 4,
+      link:
+        "https://rateyourmusic.com/release/album/kendrick-lamar/good-kid-m_a_a_d-city-6/"
+    },
+    {
+      artist: "Swans",
+      title: "The Seer",
+      decade: "2010s",
+      year: 2012,
+      stars: 5,
+      link: "https://rateyourmusic.com/release/album/swans/the-seer-2/"
+    },
+    {
+      artist: "Portishead",
+      title: "Dummy",
+      decade: "1990s",
+      year: 1994,
+      stars: 5,
+      link: "https://rateyourmusic.com/release/album/portishead/dummy/"
+    }
+  ]
+});
